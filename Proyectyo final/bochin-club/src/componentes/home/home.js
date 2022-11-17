8380732bed318ec698bb51848620d896a4a5a618
NahuelSandoval <apos_facu@hotmail.com>
2022-11-17 16:48:07 -0300

    proyectoFinal

---
+++ Proyectyo final/bochin-club/src/componentes/home/home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./home.css";
+
+const Home = () => {
+    return(
+    <header>
+        <h3> Header </h3>
+    </header>
+)
+}
+export default Home;
---
+++ Proyectyo final/bochin-club/src/componentes/navbar/navbar.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+
+const Menu = () => {
+    return (
+        <>
+            {[ 'sm' ].map((expand) => (
+                <Navbar key={expand} bg="light" expand={expand} className="mb-3">
+                    <Container fluid>
+                        <Navbar.Brand href="#">Fraternidad Bochin Club</Navbar.Brand>
+                        <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+                        <Navbar.Offcanvas
+                            id={`offcanvasNavbar-expand-${expand}`}
+                            aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+                            placement="end"
+                        >
+                            <Offcanvas.Header closeButton>
+                                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                                    Menu
+                                </Offcanvas.Title>
+                            </Offcanvas.Header>
+                            <Offcanvas.Body>
+                                <Nav className="justify-content-end flex-grow-1 pe-3">
+                                    <Nav.Link href="/">Inicio</Nav.Link>
+                                    <Nav.Link href="/">Nuestro Club</Nav.Link>
+                                    <Nav.Link href="#action2">Tienda</Nav.Link>
+                                    <Nav.Link href="/contacto">Contacto</Nav.Link>
+                                </Nav>
+                        
+                            </Offcanvas.Body>
+                        </Navbar.Offcanvas>
+                    </Container>
+                </Navbar>
+            ))}
+        </>
+    )
+}
+
+export default Menu;
